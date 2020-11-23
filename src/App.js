85c8d5308bf1bc590a740670a387fb691f83ac81
MAbdurahman <mdbdrrhm@gmail.com>
2020-11-23 06:34:27 -0600

    Add blank components for: Button, Die, and RollDie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Die from './components/die/Die';
 
 export default function App() {
   return (
     <div className='App'>
       <h1>React-Roll-Dice</h1>
+      <Die face='five' />
+      <Die face='six' />
     </div>
   );
 };
